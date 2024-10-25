88b47eab3f986db9c0c61dbb5779a09e7b9e3de1
jible <jamezman88th@gmail.com>
2024-10-25 15:51:21 -0700

    little bit of progress on page requirements

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// This stores all click 
+//events that need to be tracked. 
+//When they are added, it adds them to local storage
+const triggered = 'true';
+const untriggred = 'false';
+
+
+
+function trigger(name){
+    this.triggered = true;
+    localStorage.setItem(this.name, triggered)
+}
+
+class page{
+    name = [];
+    requirements = [];
+
+    constructor (name, req){
+        this.name = name;
+        this.req = req
+    }
+
+    checkAvailable(){
+        for (let req of this.requirements){
+            if (localStorage.getItem != triggered){  return false }
+        }
+        return true;
+    }
+}
+
+
