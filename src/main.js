7bb40f0b45f4ca035fe9657bb9515a32004dd21b
jible <jamezman88th@gmail.com>
2024-10-28 00:31:13 -0700

    pages are mostly not brken

--- src/main.js
+++ src/main.js
@@ -1,34 +1,48 @@
 // This stores all click 
 //events that need to be tracked. 
 //When they are added, it adds them to local storage
-document.title = "Updated Page Title";
-
-const triggered = 'true';
-const untriggred = 'false';
-
-
-
-function trigger(name){
-    this.triggered = true;
-    localStorage.setItem(this.name, triggered)
-}
-
-class page{
-    name = [];
-    requirements = [];
-
-    constructor (name, req){
-        this.name = name;
-        this.req = req
+document.addEventListener("DOMContentLoaded", () => {
+    modPage();
+});
+function modPage(){
+    const wikiName = "My Awesome Wiki";
+    document.title = "Updated Page Title";
+    
+    
+    const wikiHeader = document.getElementById("wikiHeader");
+    if (wikiHeader){
+        console.log ('yuh');
+        wikiHeader.innerHTML = `<a href="./home.html">${wikiName}</a>`
     }
-
-    checkAvailable(){
-        for (let req of this.requirements){
-            if (localStorage.getItem != triggered){  return false }
+    const triggered = 'true';
+    const untriggred = 'false';
+    
+    
+    
+    function trigger(name){
+        this.triggered = true;
+        localStorage.setItem(this.name, triggered)
+    }
+    
+    class page{
+        name = [];
+        requirements = [];
+    
+        constructor (name, req){
+            this.name = name;
+            this.req = req
         }
-        return true;
+    
+        checkAvailable(){
+            for (let req of this.requirements){
+                if (localStorage.getItem != triggered){  return false }
+            }
+            return true;
+        }
     }
+    
+    
+    console.log("hi!");
 }
 
 
-console.log("hi!");
